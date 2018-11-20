2a2d80a55d65f95533d2ee5e5a598f14a83a5cb9
Idrees Kamal <ikamal97@gmail.com>
2018-11-19 18:33:32 -0600

    Initial Commit

---
+++ Sleepify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sleepify
+//
+//  Created by Idrees Kamal on 11/19/18.
+//  Copyright © 2018 Idrees Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
